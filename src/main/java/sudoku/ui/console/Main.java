971c0372c6cdc0d006d2fa5af1653c95f96f69b0
angelo aguilar-morillo <angelo.aguilar-morillo@student.howest.be>
2022-02-16 16:47:33 +0100

    made console and gui package

---
+++ src/main/java/sudoku/ui/console/Main.java
@@ -0,0 +1,7 @@
+package sudoku.ui.console;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
--- src/main/java/sudoku/ui/gui/Main.java
+++ src/main/java/sudoku/ui/gui/Main.java
@@ -1,4 +1,4 @@
-package SudokuSolver;
+package sudoku.ui.gui;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
