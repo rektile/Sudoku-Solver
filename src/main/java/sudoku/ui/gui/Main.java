f5f1b2dda294381f6ec77057eeea6c25bfd44722
angelo aguilar-morillo <angelo.aguilar-morillo@student.howest.be>
2023-01-14 03:21:03 +0100

    show simple interface when starting

--- src/main/java/sudoku/ui/gui/Main.java
+++ src/main/java/sudoku/ui/gui/Main.java
@@ -1,6 +1,9 @@
 package sudoku.ui.gui;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -10,6 +13,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/SudokuView.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
