9d2165c01cc48c5b649070e1310db50f56dae54e
angelo aguilar-morillo <angelo.aguilar-morillo@student.howest.be>
2023-01-22 22:20:44 +0100

    added window title

--- src/main/java/sudoku/ui/gui/Main.java
+++ src/main/java/sudoku/ui/gui/Main.java
@@ -15,6 +15,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/SudokuView.fxml"));
         Scene scene = new Scene(root);
+        primaryStage.setTitle("Sudoku");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
