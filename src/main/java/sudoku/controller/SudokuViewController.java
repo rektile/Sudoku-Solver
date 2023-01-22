134f608f19a6e34df5bc16fcff86f089dc90eb77
angelo aguilar-morillo <angelo.aguilar-morillo@student.howest.be>
2023-01-22 22:19:49 +0100

    made textformatter for numbers only

--- src/main/java/sudoku/controller/SudokuViewController.java
+++ src/main/java/sudoku/controller/SudokuViewController.java
@@ -1,13 +1,22 @@
 package sudoku.controller;
 
-import javafx.scene.control.TextArea;
+
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.UnaryOperator;
+import java.util.regex.Pattern;
+
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextFormatter;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.GridPane;
+import sudoku.classes.Sudoku;
 
 public class SudokuViewController {
+
+    private static final Sudoku sudokuManager = new Sudoku();
 
     @FXML
     private ResourceBundle resources;
@@ -18,23 +27,37 @@ public class SudokuViewController {
     @FXML
     private GridPane gridSudoku;
 
-    @FXML
-    void OnFieldTyped(KeyEvent event) {
-        String input = event.getCharacter();
-        System.out.println(event);
-        TextArea textArea = (TextArea) event.getTarget();
-        String curValue = textArea.getText();
-        System.out.println(curValue);
-        if (input.matches("[0-9]")) {
-            textArea.setText(input);
-        }else{
-            textArea.setText(curValue);
-        }
+    private TextFormatter getNumberOnlyFormatter(){
+        UnaryOperator<TextFormatter.Change> filter = change -> {
+            String newChar = change.getText();
+            String fullString = change.getControlNewText();
+
+            if(fullString.length() <= 1 && newChar.matches("[0-9]")){
+                return change;
+            }else if(newChar.equals("")){
+                return change;
+            }else {
+                return null;
+            }
+
+        };
+
+        return new TextFormatter<>(filter);
     }
 
     @FXML
     void initialize() {
         assert gridSudoku != null : "fx:id=\"gridSudoku\" was not injected: check your FXML file 'SudokuView.fxml'.";
 
+        for (int i = 0; i < gridSudoku.getChildren().size(); i++) {
+            Node node = gridSudoku.getChildren().get(i);
+            if (node instanceof TextArea) {
+                TextArea textArea = (TextArea) node;
+                TextFormatter filter = getNumberOnlyFormatter();
+                textArea.setTextFormatter(filter);
+            }
+        }
+
     }
+
 }
